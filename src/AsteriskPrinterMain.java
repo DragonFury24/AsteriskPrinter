e3b0e9dc0671e9f945d6aa3374511e0e23fe8927
Duy <dennisdnguyen19@gmail.com>
2017-10-31 18:36:02 -0400

    Change switch to ints

--- src/AsteriskPrinterMain.java
+++ src/AsteriskPrinterMain.java
@@ -7,33 +7,31 @@ public class AsteriskPrinterMain {
     }
 
     static void askAndDraw(Scanner keyType) {
-        String input;
         while (true) {
             System.out.println("Please type in the number of the option you would like to choose.");
             AsteriskDrawer.displayOptions();
-            input = keyType.next();
-            switch (input) {
-                case "1": //Box
+            switch (keyType.nextInt()) {
+                case 1: //Box
                     AsteriskDrawer.askForNumRows();
                     AsteriskDrawer.drawBox(keyType.nextInt());
                     break;
-                case "2": //Left-aligned-triangle
+                case 2: //Left-aligned-triangle
                     AsteriskDrawer.askForNumRows();
                     AsteriskDrawer.drawLeftAlignedTriangle(keyType.nextInt());
                     break;
-                case "3": //Right-center-triangle
+                case 3: //Right-center-triangle
                     AsteriskDrawer.askForNumRows();
                     AsteriskDrawer.drawRightAlignedTriangle(keyType.nextInt());
                     break;
-                case "4": //Center triangle
+                case 4: //Center triangle
                     AsteriskDrawer.askForNumRows();
                     AsteriskDrawer.drawCenterAlignedTriangle(keyType.nextInt());
                     break;
-                case "5": //Box with X
+                case 5: //Box with X
                     AsteriskDrawer.askForNumRows();
                     AsteriskDrawer.drawBoxWithX(keyType.nextInt());
                     break;
-                case "6": //Exit
+                case 6: //Exit
                     return;
                 default:
                     System.out.println("Not a valid option.");
